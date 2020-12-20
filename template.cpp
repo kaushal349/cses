ddcca7b166fdf1e05bca8a593e1d0e97c6dcc8fe
kaushal <kaushalaggarwal349@gmail.com>
2020-12-20 11:59:40 +0530

    template added

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define rep(i,n) for (i = 0; i < n; ++i) 
+#define REP(i,k,n) for (i = k; i <= n; ++i) 
+#define REPR(i,k,n) for (i = k; i >= n; --i)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define mod 1e9+7
+#define present(c,x) ((c).find(x)!=(c).end())
+typedef vector<int> vi;
+typedef vector<ll> vll; 
+void solve();
+// Driver Program 
+int main() 
+{ 
+	ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+  	cout.tie(NULL);
+#ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("error.txt", "w", stderr); 
+    freopen("output.txt", "w", stdout); 
+#endif 
+  
+    int t = 1; 
+    /*is Single Test case?*/ cin >> t; 
+    while (t--) { 
+        solve(); 
+        cout<<"\n";
+    } 
+  
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
+    return 0; 
+} 
+
+
+void solve(){
+	
+}
